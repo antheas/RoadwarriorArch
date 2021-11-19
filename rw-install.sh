23f83cd70af33eee7ac571fb9274a1ff6cc02372
antheas <antheas@users.noreply.github.com>
2021-11-19 17:09:59 +0100

    rename 2 script, add one for dotfiles

--- rw-install.sh
+++ rw-install.sh
@@ -13,6 +13,21 @@ arch-chroot /mnt /root/install-script/1-setup.sh
 source /mnt/root/install-script/install.conf
 arch-chroot /mnt rm -R -f /home/$username/install-script
 arch-chroot /mnt cp -R /root/install-script /home/$username/install-script
+rm -f -r /root/install-script
 arch-chroot /mnt chown -R $username /home/$username/install-script
 
-# arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/install-script/2-user.sh
+echo "System is now bootable"
+echo "Next step: running 2-software.sh and 3-dotfiles.sh"
+echo "They can also be run after booting from the dir ~/install-script"
+read -p "Install now? (y/N):" installNow
+case $installNow in
+  y|Y|yes|Yes|YES)
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/install-script/2-software.sh
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/install-script/3-dotfiles.sh
+    ;;
+  *)
+    echo "Exiting..."
+    exit 1
+    ;;
+esac
+# 
